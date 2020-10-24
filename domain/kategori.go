34938e9403cbd4a3151eb824ce7e6054b73af2d3
Ilham Surya Pratama <ilhamsurya26@gmail.com>
2020-10-24 12:01:04 +0700

    add get by id method

--- domain/kategori.go
+++ domain/kategori.go
@@ -24,10 +24,12 @@ func (Kategori) TableName() string {
 type KategoriUsecase interface {
 	Get(ctx context.Context) ([]Kategori, error)
 	Create(context.Context, *Kategori) error
+	GetByID(ctx context.Context, id int) (Kategori, error)
 }
 
 // KategoriRepository ...
 type KategoriRepository interface {
 	Get(ctx context.Context) (res []Kategori, err error)
 	Create(ctx context.Context, k *Kategori) error
+	GetByID(ctx context.Context, id int) (Kategori, error)
 }
