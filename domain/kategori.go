a3f5c7e510e77edff0997c74b7d49ac0fea7849a
Ilham Surya Pratama <ilhamsurya26@gmail.com>
2020-10-24 10:48:49 +0700

    create kategorin (domain)

---
+++ domain/kategori.go
@@ -0,0 +1,31 @@
+package domain
+
+import (
+	"context"
+)
+
+// Kategori ...
+type Kategori struct {
+	IDKategoriProduk int    `gorm:"primaryKey" tag:"id_kategori_produk" json:"id"`
+	NamaKategori     string `tag:"nama_kategori" json:"nama_kategori"`
+}
+
+// Tabler ...
+type Tabler interface {
+	TableName() string
+}
+
+// TableName ...
+func (Kategori) TableName() string {
+	return "kategori_produk"
+}
+
+// KategoriUsecase ...
+type KategoriUsecase interface {
+	Get(ctx context.Context) ([]Kategori, error)
+}
+
+// KategoriRepository ...
+type KategoriRepository interface {
+	Get(ctx context.Context) (res []Kategori, err error)
+}
