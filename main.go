cf8944cda0c63ceee4e600a25900e076814c6f58
Ilham Surya Pratama <ilhamsurya26@gmail.com>
2020-10-24 10:49:49 +0700

    fixing main.go with initiate gin

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"mpnj-api/config"
+	"mpnj-api/handler"
+	"mpnj-api/repositories"
+	"mpnj-api/usecase"
+
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
-	return
+	r := gin.New()
+	db := config.Connect()
+
+	repo := repositories.NewKategoriRepository(db)
+	usecase := usecase.NewKategoriUseCase(repo)
+
+	api := r.Group("/api")
+	handler.KategoriHandlerFunc(api, usecase)
+	r.Run()
 }
