60e3675576aa4586d5941bb91818c95898db988f
Ilham Surya Pratama <ilhamsurya26@gmail.com>
2020-11-01 17:17:00 +0700

    ignore password field in user

--- domain/User.go
+++ domain/User.go
@@ -5,7 +5,7 @@ type User struct {
 	IDUser      int    `gorm:"primaryKey" tag:"id_user" json:"id"`
 	NamaLengkap string `tag:"nama_lengkap" form:"nama_lengkap" json:"nama_lengkap"`
 	Username    string `tag:"username" form:"username" json:"username"`
-	Password    string `tag:"password" form:"password" json:"password"`
+	Password    string `gorm:"-" tag:"password" form:"password" json:"password"`
 	NomorHp     string `tag:"nomor_hp" form:"nomor_hp" json:"nomor_hp"`
 	Email       string `tag:"email" form:"email" json:"email"`
 }
