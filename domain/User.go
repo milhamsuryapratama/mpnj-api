8c5300125dfbb5bd870fa85f98faef8a93558c93
Ilham Surya Pratama <ilhamsurya26@gmail.com>
2020-11-01 15:46:20 +0700

    add domain user

---
+++ domain/User.go
@@ -0,0 +1,11 @@
+package domain
+
+// User ...
+type User struct {
+	IDUser      int    `gorm:"primaryKey" tag:"id_user" json:"id"`
+	NamaLengkap string `tag:"nama_lengkap" form:"nama_lengkap" json:"nama_lengkap"`
+	Username    string `tag:"username" form:"username" json:"username"`
+	Password    string `tag:"password" form:"password" json:"password"`
+	NomorHp     string `tag:"nomor_hp" form:"nomor_hp" json:"nomor_hp"`
+	Email       string `tag:"email" form:"email" json:"email"`
+}
