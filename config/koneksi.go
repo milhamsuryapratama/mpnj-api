bf653c40fa04696d2a0dd3fe33e0af479087986b
Ilham Surya Pratama <ilhamsurya26@gmail.com>
2020-12-10 14:25:38 +0700

    test set db config manually

--- config/koneksi.go
+++ config/koneksi.go
@@ -1,19 +1,16 @@
 package config
 
 import (
-	"github.com/joho/godotenv"
-	"os"
-
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 // Connect ..
 func Connect() *gorm.DB {
-	godotenv.Load("./.env")
-	dbName := os.Getenv("MYSQL_DB")
-	dbUser := os.Getenv("MYSQL_USER")
-	dbPassword := os.Getenv("MYSQL_PASSWORD")
+	//godotenv.Load("./.env")
+	dbName := "mpnj"
+	dbUser := "root"
+	dbPassword := ""
 	dsn := dbUser + ":" + dbPassword + "@tcp(127.0.0.1:3306)/" + dbName + "?charset=utf8mb4&parseTime=True&loc=Local"
 	db, _ := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
