617d470f628fe7b3006d1d13e5b7ed8828e2604f
Ilham Surya Pratama <ilhamsurya26@gmail.com>
2021-01-20 22:12:18 +0700

    fixing produk domain
    - add ProdukWithUser struct
    - delete ProdukWithKategori struct

--- domain/produk.go
+++ domain/produk.go
@@ -19,18 +19,18 @@ type Produk struct {
 	Wishlist         int      `tag:"wishlist" form:"wishlist" json:"wishlist"`
 	Terjual          int      `tag:"terjual" form:"terjual" json:"terjual"`
 	KategoriProdukID int      `sql:"index" form:"kategori_produk_id" tag:"kategori_produk_id" json:"kategori_produk_id"`
+	Kategori         Kategori `gorm:"foreignKey:kategori_produk_id" tag:"kategori" json:"kategori"`
 	Status           string   `tag:"status" form:"status" json:"status"`
 }
 
-type ProdukWithKategori struct {
-	Produk
-	Kategori         Kategori `gorm:"foreignKey:kategori_produk_id" tag:"kategori" json:"kategori"`
-	User             User     `gorm:"foreignKey:user_id" tag:"user" json:"penjual"`
+type ProdukWithUser struct {
+	Produk Produk `json:"produk"`
+	User   User   `gorm:"foreignKey:user_id" tag:"user" json:"user"`
 }
 
 // ProdukUsecase ...
 type ProdukUsecase interface {
-	Get(ctx context.Context) ([]ProdukWithKategori, error)
+	Get(ctx context.Context) ([]Produk, error)
 	Create(context.Context, *Produk) error
 	GetByID(ctx context.Context, id int) (Produk, error)
 	Update(produk *Produk, id int) (Produk, error)
@@ -39,7 +39,7 @@ type ProdukUsecase interface {
 
 // ProdukRepository ...
 type ProdukRepository interface {
-	Get(ctx context.Context) (res []ProdukWithKategori, err error)
+	Get(ctx context.Context) (res []Produk, err error)
 	Create(ctx context.Context, p *Produk) error
 	GetByID(ctx context.Context, id int) (Produk, error)
 	Update(produk *Produk, id int) (Produk, error)
